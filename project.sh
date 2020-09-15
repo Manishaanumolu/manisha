ed2dbfcbbbb160c3cfc3cd779f4522355e5cf74f
Manishaanumolu <71320605+Manishaanumolu@users.noreply.github.com>
2020-09-15 15:59:56 +0530

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,16 +1,1 @@
-pipeline {
-    agent any
-
-    stages {
-        stage('Git Check out') {
-            steps {
-            git poll: false, 'https://github.com/xxxxxx' 
-            }
-        }
-        stage('suceess') {
-        steps {
-          echo 'After checkout this is is next stage'
-        }
-    }
-  }
-}
+echo "i am learning jenkins"
